10b4cb09822f26efeeb46a51fc77ecdb9f1df358
=Wajuma <=meltusjuma5@gmail.com>
2022-09-30 15:51:18 +0300

    removed favicon routes

--- src/routes/favicon_route.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-from src.controllers.favicon_controller import icon
-
-favicon =Blueprint('favicon', __name__)
- 
-favicon.route('favicon.png')(icon)
-favicon.route('favicon.ico')(icon)
