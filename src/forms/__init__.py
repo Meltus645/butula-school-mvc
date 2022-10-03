4856f2e5cd22c432efef28df5f16902a5c186f2e
=Wajuma <=meltusjuma5@gmail.com>
2022-10-03 21:49:23 +0300

    created forms

--- src/forms/__init__.py
+++ src/forms/__init__.py
@@ -1,3 +1,4 @@
-from .student_form import student_form
+from .student_form import StudentForm
 from .resource_form import resource_form
 from .staff_form import staff_form
+from .timetable_form import TimetableForm
--- src/forms/student_form.py
+++ src/forms/student_form.py
@@ -1,16 +1,17 @@
-from src.models import StudentsModel  
-from flask_mongoengine.wtf import model_form
+from flask_wtf import FlaskForm 
+from wtforms import StringField, SelectField, FileField
+from src.models import ClassesModel
 
- 
+class StudentForm(FlaskForm):
+    name =StringField(label ="Student Name", name='name', id='name')
+    phone =StringField(label ="Phone Number", name='phone', id='phone')
+    admission_number =StringField(label ="Admission Number", name='admission_number', id='admission_number')
+    birth_certificate_number =StringField(label ="Birth Certificate Number", name='birth_certificate_number', id='birth_certificate_number')
+    form =SelectField(label ="Student Class", name='form', id='form')
+    avatar =FileField(label ="Passport Photo", name='avatar', id='avatar')
 
-student_form =model_form(
-    model =StudentsModel,
-    exclude=[
-        'subjects', 
-        'status',
-        'password',
-        'gender',
-        'enrolled'
-    ]
-)
-         
+    def __init__(self, **kwargs):
+        super(StudentForm, self).__init__(**kwargs)
+        class_choices =[(cls.id, cls) for cls in ClassesModel.objects.all()]  
+        self.form.choices =[('', 'Select student class'), *class_choices]
+  
