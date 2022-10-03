a5132fcb06950fa08fc4bdb8a0497cab2e8a9f0b
=Wajuma <=meltusjuma5@gmail.com>
2022-10-03 21:53:43 +0300

    removed save overide, file saving moved to model_service

--- src/models/resourcesModel.py
+++ src/models/resourcesModel.py
@@ -18,9 +18,6 @@ class ResourcesModel(Document):
 
     meta ={'collection': 'resources', 'indexes': [{'fields': ['$topic']}]}
 
-    def save(self, force_insert=False, validate=True, clean=True, write_concern=None, cascade=None, cascade_kwargs=None, _refs=None, save_condition=None, signal_kwargs=None, **kwargs):
-        return super().save(force_insert, validate, clean, write_concern, cascade, cascade_kwargs, _refs, save_condition, signal_kwargs, **kwargs)
-
-    
+     
     def __str__(self):
         return f'{self.topic}'
