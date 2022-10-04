2ff4a2a006b9ccb579120076a1fc4fd66531322c
=Wajuma <=meltusjuma5@gmail.com>
2022-10-04 16:47:45 +0300

    added a media fen url controller

--- src/controllers/static_controller.py
+++ src/controllers/static_controller.py
@@ -1,4 +1,9 @@
-from flask import send_from_directory, current_app, request
+from flask import send_from_directory, current_app 
+from src.utils.constants.app import BASEDIR
 
-def get_file():  
-    return send_from_directory(current_app.static_folder, request.path[1:]) 
+def get_file(path:str):   
+    return send_from_directory(current_app.static_folder, path) 
+
+def get_media(dir:str, path:str): 
+    print(dir, path)
+    return send_from_directory(BASEDIR, f'media/{dir}/{path}')
