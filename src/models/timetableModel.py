2714583442030681006e299416c719c1345149a7
=Wajuma <=meltusjuma5@gmail.com>
2022-10-04 16:48:58 +0300

    added file string field

--- src/models/timetableModel.py
+++ src/models/timetableModel.py
@@ -4,10 +4,10 @@ from .classesModel import ClassesModel
 
 class TimetableModel(Document):
     form =ReferenceField(ClassesModel, required =True)
-    purpose =StringField(required =True, choices =(('Teaching', 'Teaching'),('Exams', 'Exams')), default ='Teaching') 
-    content =StringField(required =True, max_length=64) 
+    purpose =StringField(required =True, max_length=32) 
+    file =StringField(required =True, max_length=64) 
 
     meta ={'collection': 'timetable', 'indexes': [{'fields': ['$form', '$purpose'], 'default_language': 'english'}]}
 
     def __str__(self):
-        return f'{self.form.name} {self.purpose} timetable'
+        return f'{self.form} {self.purpose} timetable'
