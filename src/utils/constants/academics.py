5d88ee74d379704be16dae422b1173d0028a9d37
=Wajuma <=meltusjuma5@gmail.com>
2022-10-06 20:47:45 +0300

    updated form, fields

--- src/utils/constants/academics.py
+++ src/utils/constants/academics.py
@@ -1,6 +1,6 @@
 from flask_mongoengine.wtf import model_form   
 from src.models import ClassesModel, ExamsModel, ResourcesModel, SchoolCalendarModel, SubjectsModel, TimetableModel
-from src.forms import resource_form, TimetableForm
+from src.forms import ResourceForm, TimetableForm, ExamForm, SubjectForm, ClassesForm, CalendarForm
 
 ACADEMIC_SECTIONS =['e-notes', 'subjects', 'exams', 'classes','time-table', 'school-calendar'] 
 
@@ -8,8 +8,8 @@ ACADEMIC_FIELDS ={
     'subjects': ['code', 'name'],
     'e-notes': ['topic', 'subject', 'views', 'downloads', 'author', 'time_uploaded'],
     'exams': ['exam_name', 'exam_type'],
-    'time-table': ['form', 'purpose', 'file'],
-    'school-calendar': [],
+    'time-table': ['form', 'purpose', 'filename'],
+    'school-calendar': ['year', 'term', 'starts_from', 'ends_on'],
     'classes': ['form', 'stream']
 }
  
@@ -18,13 +18,19 @@ ACADEMIC_PLACEHOLDERS ={
         'code': 'Enter subject code e.g 101',
         'name': 'Enter subject name e.g english'
     },
-    'e-notes': {},
+    'e-notes': {
+        'topic': 'enter resource topic', 
+        'description': 'Provide resource description or instructions'
+    },
     'exams': {
         'exam_name': 'Enter Exam Name e.g C.A.T 1', 
     },
     'time-table': {},
-    'school-calendar': {},
-    'classes': {'form': 'Enter class level e.g 1'}
+    'school-calendar': {
+        'year': 'Enter current academic year',
+        'term': 'Enter current term'
+    },
+    'classes': {'form': 'Enter class level e.g 1', 'stream': 'Enter stream name'}
 
 }
 
@@ -38,10 +44,10 @@ ACADEMIC_MODELS ={
 }
 
 ACADEMIC_FORMS ={
-    'subjects': model_form(SubjectsModel),
-    'e-notes': resource_form,
-    'exams': model_form(ExamsModel),
+    'subjects': SubjectForm,
+    'e-notes': ResourceForm,
+    'exams': ExamForm,
     'time-table': TimetableForm,
-    'school-calendar': model_form(SchoolCalendarModel),
-    'classes': model_form(ClassesModel)
+    'school-calendar': CalendarForm,
+    'classes': ClassesForm
 }
