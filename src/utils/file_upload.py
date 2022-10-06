e9877e7d706b5c400fb56b7e965e27a4583c630a
=Wajuma <=meltusjuma5@gmail.com>
2022-10-06 20:49:01 +0300

    added base64 conversion

--- src/utils/file_upload.py
+++ src/utils/file_upload.py
@@ -1,9 +1,9 @@
 from .mediaWorker import MediaWorker
 
-def upload_file(file, _type='img', swap_with:str =None)->tuple:
-    worker = MediaWorker(file, _type) 
+def upload_file(file, _type='img', swap_with:str =None, as_b64 =False)->tuple:
+    worker = MediaWorker(file, _type)  
     if swap_with:
-        done, filepath =worker.upload_media()
+        done, filepath =worker.upload_media(as_b64 =as_b64)
         if done: return worker.remove_media(swap_with) 
         return done, filepath
-    return worker.upload_media() 
+    return worker.upload_media(as_b64 =as_b64)  
--- src/utils/mediaWorker.py
+++ src/utils/mediaWorker.py
@@ -1,6 +1,7 @@
 from werkzeug.utils import secure_filename
 from uuid import uuid1  
-import os 
+import os, base64
+import filetype
 
 from .constants.app import BASEDIR
 
@@ -8,16 +9,17 @@ class MediaWorker:
     def __init__(self, media, expected:str )->tuple: 
         self.media =media 
         self.allowed_metadata ={'img':  {'ext': ('png', 'jpg', 'jpeg'), 'mime': ('image/png', 'image/jpeg', 'image/jpg')}, 'doc': {'ext': ('pdf',), 'mime': ('application',)}, 'vid': {'ext': ('mp4',), 'mime': ('video',)}}
-        
+        self.filename =secure_filename(filename=self.media.filename).lower()
         self.expected_extensions =self.allowed_metadata[expected]['ext']
         self.expected_mimetypes  =self.allowed_metadata[expected]['mime']
         self.upload_dir =expected
         self.max_size =1024 *1024 
-        self.media_root =BASEDIR /'media'   
+        self.media_root =BASEDIR /'media'  
 
-    def upload_media(self):
+    def upload_media(self, as_b64=False):
         try:  
             filename =self.validate_media()  
+            if as_b64: return self.encode_media_to_base64()
             return self.save_media(filename=filename) 
         except Exception as e: return False, f'{e}'
         
@@ -27,17 +29,23 @@ class MediaWorker:
             return True, None 
         except Exception as e: return False, f'{e}'
 
-    def validate_media(self)->str: 
-        filename =secure_filename(filename=self.media.filename).lower() 
-        file_extension =filename.split('.')[-1].strip() 
+    def validate_media(self)->str:  
+        self.blob  =self.media.read()  
+        file_extension =self.filename.split('.')[-1].strip() 
         mimetype =self.media.mimetype.lower()  
         if not file_extension in self.expected_extensions or not mimetype in self.expected_mimetypes: raise TypeError(f"Only {','.join(self.expected_extensions)} are allowed.") # extension and 
-        # elif len(self.media.read()) >self.max_size: raise ValueError("413: file larger than 1mb") 
-        return filename
+        elif len(self.blob) >self.max_size: raise ValueError("413: file larger than 1mb") 
+        return self.filename
 
     def save_media(self, filename:str)->tuple: 
         try:  
             mediapath =f'{self.upload_dir}/{uuid1()}_{filename}' 
-            self.media.save(dst =self.media_root /mediapath)   
+            # self.media.save(dst =self.media_root /mediapath)   
+            with open(self.media_root /mediapath, 'wb') as fs: fs.write(self.blob)
             return True, mediapath
         except Exception as e: return False, f'{e}' 
+    
+    def encode_media_to_base64(self):
+        try:   
+            return True, base64.b64encode(self.blob).decode() 
+        except Exception as e: False, f'{e}'
