8cde24eef5996eea96891e38ec2cf8c6f3734c4d
=Wajuma <=meltusjuma5@gmail.com>
2022-10-07 07:19:22 +0300

    added timeago to datetimefields

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,4 +1,5 @@
-from .filters import properties, slugify
+from .filters import  slugify,properties, since, type_of,strip_special
+
 
 def load_filters(app)->None:
     """
@@ -9,4 +10,7 @@ def load_filters(app)->None:
     """
     app.jinja_env.filters['properties'] =properties
     app.jinja_env.filters['slugify'] =slugify
+    app.jinja_env.filters['since'] =since
+    app.jinja_env.filters['type_of'] =type_of
+    app.jinja_env.filters['strip_special'] =strip_special
       
--- src/utils/filters.py
+++ src/utils/filters.py
@@ -1,9 +1,12 @@
-import re
+import re, timeago 
+from datetime import datetime
 
 def slugify(url:str):
     if type(url) is str: return re.sub('\s+', '-', str(url).strip())
     elif type(url) is dict: return {list(url.keys())[0]: slugify(list(url.values())[0])}
-    else: return url 
+    else: return url  
 
-
-properties =lambda cls: dir(cls)
+properties =lambda cls: dir(cls) # calls dir() on a class, cls
+type_of =lambda obj: type(obj).__name__ 
+strip_special =lambda text: re.sub('[_\W]+', ' ', str(text).strip())
+since =lambda then: timeago.format(then, datetime.now()) # time difference between now and time_uploaded 
