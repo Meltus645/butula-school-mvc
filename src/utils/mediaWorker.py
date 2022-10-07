6210a916fc3e20b1ae98675a46818e0ac22cb35b
=Wajuma <=meltusjuma5@gmail.com>
2022-10-07 08:53:24 +0300

    created custom exceptions to handle file validateons

--- src/utils/mediaWorker.py
+++ src/utils/mediaWorker.py
@@ -1,14 +1,14 @@
 from werkzeug.utils import secure_filename
 from uuid import uuid1  
 import os, base64
-import filetype
+from .exceptions import EmptyFileError, LargeFileError, UnSupportedFileError, UnreadableFileError
 
 from .constants.app import BASEDIR
 
 class MediaWorker: 
     def __init__(self, media, expected:str )->tuple: 
         self.media =media 
-        self.allowed_metadata ={'img':  {'ext': ('png', 'jpg', 'jpeg'), 'mime': ('image/png', 'image/jpeg', 'image/jpg')}, 'doc': {'ext': ('pdf',), 'mime': ('application',)}, 'vid': {'ext': ('mp4',), 'mime': ('video',)}}
+        self.allowed_metadata ={'img':  {'ext': ('png', 'jpg', 'jpeg'), 'mime': ('image/png', 'image/jpeg')}, 'doc': {'ext': ('pdf',), 'mime': ('application/pdf',)}, 'vid': {'ext': ('mp4',), 'mime': ('video',)}}
         self.filename =secure_filename(filename=self.media.filename).lower()
         self.expected_extensions =self.allowed_metadata[expected]['ext']
         self.expected_mimetypes  =self.allowed_metadata[expected]['mime']
@@ -18,10 +18,10 @@ class MediaWorker:
 
     def upload_media(self, as_b64=False):
         try:  
-            filename =self.validate_media()  
+            filename =self.validate_media() 
             if as_b64: return self.encode_media_to_base64()
             return self.save_media(filename=filename) 
-        except Exception as e: return False, f'{e}'
+        except Exception as e: return False, e
         
     def remove_media(self, file:str):
         try:    
@@ -29,13 +29,16 @@ class MediaWorker:
             return True, None 
         except Exception as e: return False, f'{e}'
 
-    def validate_media(self)->str:  
-        self.blob  =self.media.read()  
-        file_extension =self.filename.split('.')[-1].strip() 
-        mimetype =self.media.mimetype.lower()  
-        if not file_extension in self.expected_extensions or not mimetype in self.expected_mimetypes: raise TypeError(f"Only {','.join(self.expected_extensions)} are allowed.") # extension and 
-        elif len(self.blob) >self.max_size: raise ValueError("413: file larger than 1mb") 
-        return self.filename
+    def validate_media(self)->str:
+        try:  
+            self.blob  =self.media.read()   
+            if len(self.blob) ==0: raise EmptyFileError() 
+            file_extension =self.filename.split('.')[-1].strip() 
+            mimetype =self.media.mimetype.lower()  
+            if not file_extension in self.expected_extensions or not mimetype in self.expected_mimetypes: raise UnSupportedFileError(f"Only {','.join(self.expected_extensions)} are allowed.") # extension and 
+            if len(self.blob) >self.max_size: raise LargeFileError("file larger than 1mb") 
+            return self.filename
+        except IOError: raise UnreadableFileError()
 
     def save_media(self, filename:str)->tuple: 
         try:  
