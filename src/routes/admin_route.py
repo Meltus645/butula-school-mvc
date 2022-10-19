a3249b2b20672331b25add4307563ff2582fc54a
=Wajuma <=meltusjuma5@gmail.com>
2022-10-19 19:15:49 +0300

    changed routes for admin

--- src/routes/admin_route.py
+++ src/routes/admin_route.py
@@ -1,20 +1,14 @@
 from flask import Blueprint 
 
 from src.utils.constants.app import BASEDIR
-from src.controllers.admin_controller import dashboard, users, error_404, academics, settings, subscribers, support, error_403 
+from src.controllers.admin_controller import controller 
  
 admin =Blueprint('admin', __name__, template_folder= BASEDIR /'templates/admin') 
 
-admin.route('/')(dashboard)
-admin.route('/manage/<string:position>')(users)
-admin.route('/academics')(academics)
-admin.route('/academics/<string:section>')(academics)
-admin.route('/academics/<string:section>/<string:action>', methods =['GET', 'POST'])(academics)
-admin.route('/academics/<string:section>/<string:action>/<string:id>', methods =['GET', 'POST'])(academics)
-admin.route('/manage/<string:position>/<string:action>', methods =['GET', 'POST'])(users) 
-admin.route('/manage/<string:position>/<string:action>/<string:id>', methods =['GET', 'POST'])(users) 
-admin.route('/mailing-list')(subscribers)
-admin.route('/support')(support)
-admin.route('/settings')(settings)
-admin.route('/404')(error_404)
-admin.route('/403')(error_403)
+admin.route('/')(controller)
+admin.route('/<string:page>')(controller)
+admin.route('/<string:page>/<string:section>')(controller)
+admin.route('/<string:page>/<string:section>/<string:action>', methods =['GET', 'POST'])(controller) 
+admin.route('/<string:page>/<string:section>/<string:action>/<string:id>', methods =['GET', 'POST'])(controller) 
+# admin.route('/404')(error_404)
+# admin.route('/403')(error_403)
