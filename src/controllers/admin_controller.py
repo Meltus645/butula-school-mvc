7254cc83a63009d35e00379b51bd8fb0a5c483b1
=Wajuma <=meltusjuma5@gmail.com>
2022-10-19 18:58:27 +0300

    shift functionality to controller

--- src/controllers/admin_controller.py
+++ src/controllers/admin_controller.py
@@ -1,133 +1,65 @@
-from flask import request, render_template, redirect, url_for, jsonify   
-from src.utils.constants.academics import ACADEMIC_FIELDS, ACADEMIC_FORMS, ACADEMIC_PLACEHOLDERS, ACADEMIC_SECTIONS, ACADEMIC_MODELS, ACADEMIC_FILES 
-from src.utils.constants.app import ACTIONS
-from src.utils.constants.users import USER_FORMS, USER_MODELS, USER_POSITIONS, USER_PLACEHOLDERS, USER_FIELDS
-from src.services.requests_service import RequestsService
-from flask_wtf import FlaskForm
 from wtforms import FloatField 
+from flask import request, render_template, jsonify   
 from src.forms import ResultsForm, SubjectsSelectionForm
+from src.services.requests_service import RequestsService 
+from src.utils.exceptions import PageNotFoundError 
+from src.utils.constants.app import ACTIONS 
+from src.utils.mappers import page_mapper
 
+def controller(page ='dashboard', section:str =None, action:str =None, id:str =None):
+    try: 
+        if not action: action ='list'
+        if not action in ACTIONS: raise PageNotFoundError
+        model_data, form_data, template_data =page_mapper(page =page, section =section) 
+        service =RequestsService(model=model_data.get('model'))
+        fields =model_data.get('fields') 
+        form =form_data.get('form') 
+        placeholders:dict =form_data.get('placeholders') 
+        accepts:list =form_data.get('accepts')
+        template =template_data.get('template')
+        section =template_data.get('section')
+        params =request.args
+        data =None 
+        tab =params.get('tab')
+        if page =='manage' and not tab in ['subjects', 'performance']: tab ='bio' 
+        context ={'page': page, 'section': section, 'action': action, 'tab': tab} 
 
-def dashboard():
-    return render_template('admin/base.html', page="dashboard")
+        if action =='list': 
+            data, status_code =service.get()   
+            context ={**context, 'data': data, 'fields': fields}
+            if params.get('init') =='app': return render_template('list.html', **context)
 
-def users(position:str, action='list', id =None): 
-    action =action.lower()
-    position =position.lower() 
-    if not position in USER_POSITIONS or not action in ACTIONS: return redirect(url_for('admin.error_404')), 404
+        if action =='new':    
+            if request.method =='POST': return service.post() 
+            context ={**context, 'form': form, 'placeholders': placeholders, 'accepts': accepts}
+            if params.get('init') =='app': return render_template('form.html', **context)
 
-    model =USER_MODELS[position]
-    service =RequestsService(model=model)
-    params =request.args  
-    form:FlaskForm =None 
-    data: dict =None
-    page =position
-    section =params.get('section')
-    if not section in ['subjects', 'performance']: section ='bio' 
-    
-    if action == 'list':  
-        data, status_code =service.get()
-        if params.get('init') =='app': return render_template('list.html', fields =USER_FIELDS[position], data=data, page=page, position=position, section =section, action =action), status_code 
+        if action =='edit':
+            if request.method =='POST': return service.put(id=id)    
+            data, _ =service.get(id=id)
+            context ={**context, 'form': form, 'placeholders': placeholders, 'accepts': accepts, 'data': data}
+            if params.get('init') =='app': return render_template('form.html', **context)
 
-    if action == 'new':  
-        if request.method =='POST':  
-            response, status_code =service.post()
-            return response 
-        form =USER_FORMS[position] 
-        if params.get('init') =='app': return render_template('form.html', form =form, placeholders =USER_PLACEHOLDERS[position], page=page, action =action) 
+        if action == 'view':  
+            data, _ =service.get(id=id) 
+            popup = params.get('popup') 
+            context ={**context, 'data': data}
+            field = section
+            if page =='manage': field =tab 
+            if request.method =='POST': return service.put(id =id, data={field: request.form.get(field)}) 
+            if popup:
+                if tab == 'performance':
+                    form =ResultsForm
+                    for subject in data.subjects: setattr(form, subject.name.lower(), FloatField(label=subject.name, name =f'{subject.code}'))
+                elif tab =='subjects': form =SubjectsSelectionForm  
+                context ={**context, 'data': data, 'popup': popup, 'form': form, 'placeholders': placeholders,'accepts': accepts}
+                return render_template('modal.html', **context)
+            if params.get('init') =='app': return render_template('view.html', **context)
 
-    if action == 'edit': 
-        if request.method =='POST': 
-            response, status_code =service.put(id=id)
-
-        data, status_code =service.get(id=id)    
-        form =USER_FORMS[position]
-        if params.get('init') =='app': return render_template('form.html', form =form, placeholders =USER_PLACEHOLDERS[position], page =page, action =action, position =position) 
-
-    if action == 'view':  
-        data, status_code =service.get(id=id) 
-        popup = params.get('popup') 
-        if request.method =='POST': 
-            data.subjects =request.form.get('subjects')
-            print(data.to_pymongo().keys())
-            return  service.put(id =id, data=data)
-
-        if popup:
-            if section == 'performance':
-                form =ResultsForm
-                for subject in data.subjects: setattr(form, subject.name.lower(), FloatField(label=subject.name, name =f'{subject.code}'))
-            elif section =='subjects': form =SubjectsSelectionForm  
-            return render_template('modal.html', section =section, data =data, page =page, action =action, popup =popup, form =form, position =position)
-        if params.get('init') =='app':  
-            return render_template('view.html', section =section, data =data, page =page, action =action, position =position)
-
-    if action == 'delete':
-        response, status_code =service.delete(id=id)
-        if status_code ==204: return jsonify({'deleted': True})
-        return jsonify({'deleted': False, 'detail': response['detail']})
-
-    return render_template(f'{position}.html', page=page, action =action, form =form, section =section, placeholders =USER_PLACEHOLDERS[position], fields =USER_FIELDS[position], data =data, position =position)
- 
-def academics(section ='e-notes', action ='list', id:str =None): 
-    section =section.lower()
-    action =action.lower() 
-    if not section in ACADEMIC_SECTIONS or not action in ACTIONS: return redirect(url_for('admin.error_404'))
-
-    model =ACADEMIC_MODELS[section]
-    service =RequestsService(model=model)
-    params =request.args
-    page ='academics' 
-    data =None
-    form: FlaskForm =ACADEMIC_FORMS[section] or None
-    placeholders:dict =ACADEMIC_PLACEHOLDERS[section] or None
-    accepts:list =ACADEMIC_FILES[section] 
-    fields =ACADEMIC_FIELDS[section] or None
-     
-
-    if action =='list': 
-        data, status_code =service.get()   
-        if params.get('init') =='app': return render_template('list.html', data=data, fields =fields, section =section, page =page, action =action)
-
-    if action =='new':    
-        if request.method =='POST':  
-            response, status_code =service.post()
-            return response, 400  
-        if params.get('init') =='app': return render_template('form.html', form =form, section =section, page =page, action =action, placeholders =placeholders, accepts=accepts)
-
-    if action =='edit':
-        if request.method =='POST': 
-            response, status_code =service.put(id=id) 
-            return response   
-
-        data, status_code =service.get(id=id)
-        if params.get('init') =='app': 
-            return render_template('form.html', form =form, section =section, page =page, action =action, placeholders =placeholders, data=data, accepts=accepts)
-
-    if action =='view': 
-        data, status_code =service.get(id=id)
-        if params.get('init') =='app': return render_template('list.html')
-
-    if action == 'delete':
-        response, status_code =service.delete(id=id)
-        if status_code ==204: return jsonify({'deleted': True})
-        return jsonify({'deleted': False, 'detail': response['detail']})
-     
-    return render_template('academics.html', page =page, section =section, action =action, form =form, data =data, fields =fields, placeholders =placeholders, accepts=accepts)
-
-def subscribers(): 
-    page  ='subscribers'
-    return render_template('maillist.html', page=page)
-
-def support(): 
-    page  ='support'
-    return render_template('support.html', page=page) 
-
-def settings(): 
-    page  ='settings'
-    return render_template('settings.html', page=page)
-
-def error_404():
-    return render_template('404.html')
-
-def error_403():
-    return render_template('404.html')
+        if action == 'delete':
+            response, status_code =service.delete(id=id)
+            if status_code ==204: return jsonify({'deleted': True})
+            return jsonify({'deleted': False, 'detail': response.get('detail')})
+        
+        return render_template(f'admin/{template}.html', **context)
+    except PageNotFoundError: return {'page': 'Not Found'} 
